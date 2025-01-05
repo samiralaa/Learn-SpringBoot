15e8a20177bfc359b4a3a15ab3d55c3a0e94f5ba
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2025-01-05 12:24:11 +0200

    add frist commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+//        System.out.printf("samir is new in  and welcome!");
+          System.out.printf("welcome to app");
+          System.out.printf("samir is validation to app");
+          System.out.printf("data is valid");
+
+    }
+}
+
