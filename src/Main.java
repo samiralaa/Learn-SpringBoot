4302038292ee25afdd778a0aa752385817728fe3
Samir Elzer <76245712+samiralaa@users.noreply.github.com>
2025-01-05 15:15:04 +0200

    add varable

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,10 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        String  name= "Samir Alaa";
 
-//        System.out.printf("samir is new in  and welcome!");
-          System.out.printf("welcome to app");
-          System.out.printf("samir is validation to app");
-          System.out.printf("data is valid");
-
+        String firstName = "Samir";
+        String lastName ="Alaa";
+        System.out.println("my name is " + firstName+ " " + lastName);
     }
 }
-
